7a828794d64ab868c04af10c8d375957b7a47bdf
vronic <vronic.me@gmail.com>
2015-12-19 00:42:49 +0300

    02 - bob - trade-off with only number test (

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,22 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true ->  "Whatever."
-
+    	input && String.strip(input) == "" -> "Fine. Be that way!"
+    	String.ends_with?(input, "?") -> "Sure."
+    	String.upcase(input) == input -> "Whoa, chill out!"
+    	String.ends_with?(input, "!") -> "Whatever."
+		
+    	#only_number input -> "Whatever."
+    	
+        true -> "Whatever."
     end
   end
+
+  defp only_number(input) do
+   	input
+   	|> String.split ", "
+   	|> Enum.join
+   	|> String.to_integer
+   	|> is_integer
+   end 
 end
