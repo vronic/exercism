f1eb100e72c716132d1108fa6bb45b3d874c7679
vronic <vronic.me@gmail.com>
2015-12-19 01:08:26 +0300

    02 - bob - submit

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -3,20 +3,10 @@ defmodule Bob do
     cond do
     	input && String.strip(input) == "" -> "Fine. Be that way!"
     	String.ends_with?(input, "?") -> "Sure."
-    	String.upcase(input) == input -> "Whoa, chill out!"
+    	String.downcase(input) != input && String.upcase(input) == input -> "Whoa, chill out!"
     	String.ends_with?(input, "!") -> "Whatever."
-		
-    	#only_number input -> "Whatever."
     	
         true -> "Whatever."
     end
   end
-
-  defp only_number(input) do
-   	input
-   	|> String.split ", "
-   	|> Enum.join
-   	|> String.to_integer
-   	|> is_integer
-   end 
 end
